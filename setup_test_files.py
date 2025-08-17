3eab4b8c3772a73223c1734d0e348d1c99669a0c
michal1xd <michal.lazewski.kw@gmail.com>
2025-08-17 22:12:47 +0200

    New creating logs files script

---
+++ setup_test_files.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import time
+import os
+
+def loggs():
+    logs_folder = Path('logs')
+    logs_folder.mkdir(exist_ok=True) # creating catalogues
+    
+    logs_file = logs_folder / 'new_log.txt'
+    logs_file_old = logs_folder / 'old_log.log'
+    logs_file.touch()   # creating files
+    logs_file_old.touch()
+
+    year = time.time() - (60*60*24*60)  # a year
+
+    os.utime(logs_file_old, (year, year)) # changing modification and access time of old logs
+
+
+
+
+
+
+loggs()
