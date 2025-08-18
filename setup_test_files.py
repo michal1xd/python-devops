af609935fd5774ac6c85864480560bebe973d65f
michal1xd <michal.lazewski.kw@gmail.com>
2025-08-18 21:06:23 +0200

    saving after minor changes

--- setup_test_files.py
+++ setup_test_files.py
@@ -4,6 +4,7 @@ import os
 
 def loggs():
     logs_folder = Path('logs')
+    print(logs_folder)
     logs_folder.mkdir(exist_ok=True) # creating catalogues
     
     logs_file = logs_folder / 'new_log.txt'
@@ -11,10 +12,9 @@ def loggs():
     logs_file.touch()   # creating files
     logs_file_old.touch()
 
-    year = time.time() - (60*60*24*60)  # a year
+    year = time.time() - (60*60*24*60)  # a 60 days
 
     os.utime(logs_file_old, (year, year)) # changing modification and access time of old logs
-
 
 
 
